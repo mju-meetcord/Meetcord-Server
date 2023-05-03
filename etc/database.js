177bbf3a46547c6db1a78e12c63ab240fb1281d8
jouno7201 <wjswnsdh1599@gmail.com>
2023-05-04 02:27:19 +0900

    database 수정으로 인한 쿼리부분 수정

--- etc/database.js
+++ etc/database.js
@@ -10,14 +10,14 @@ const connection = mysql.createPool({
   queueLimit: 0
 });
 
-const CreateAccount = async(data) => {
+const CreateAccount = async (data) => {
   console.log("DB create Account");
-  const sql = `INSERT INTO users (email, phone, name, password, agree, birthday)
-                    VALUES (?, ?, ?, ?, ?, ?)`;
+  const sql = `INSERT INTO users (email,phone,name, password, birthday)
+                    VALUES (?,?,?,?,?)`;
   console.log("DB request query : " + sql);
 
   try {
-    const [rows, fields] = await connection.execute(sql,data);
+    const [rows, fields] = await connection.execute(sql, data);
 
     console.log(rows);
     console.log(fields);
@@ -31,33 +31,36 @@ const CreateAccount = async(data) => {
 
 // 이메일을 받아 , DB에서 조회 후 해당하는 결과 반환
 const readAccount = async (email) => {
-    console.log("DB Read Account :" + email);
-  
-    // 이메일로 DB에서 검색 -> email은 중복이 안되도록 할 예정이기에 1개아니면 0개 반환 예상
-    const sql = 'SELECT * FROM users WHERE email = ?';
-    console.log("DB request query : " + sql);
-  
-    try {
-      const [rows, fields] = await connection.execute(sql, [email]);
-      if (rows.length) {
-        console.log(rows[0]);
-  
-        const { email, password } = rows[0];
-  
-        console.log("readAccount _ query_result_email : " + email);
-        console.log("readAccount _ query_result_pw : " + password);
-  
-        return [ email, password ];
-      } else {
-        return 0;
-      }
-    } catch (err) {
-      console.error(err);
+  console.log("DB Read Account :" + email);
+
+  // 이메일로 DB에서 검색 -> email은 중복이 안되도록 할 예정이기에 1개아니면 0개 반환 예상
+  const sql = 'SELECT * FROM users WHERE email = ?';
+  console.log("DB request query : " + sql);
+
+  try {
+    const [rows, fields] = await connection.execute(sql, [email]);
+    if (rows.length) {
+      console.log(rows[0]);
+
+      const {
+        email,
+        password
+      } = rows[0];
+
+      console.log("readAccount _ query_result_email : " + email);
+      console.log("readAccount _ query_result_pw : " + password);
+
+      return [email, password];
+    } else {
       return 0;
     }
+  } catch (err) {
+    console.error(err);
+    return 0;
+  }
 };
 
-const updateAccount =async (email) => {
+const updateAccount = async (email) => {
   console.log("DB update Account :" + email);
 
   // 이메일로 DB에서 검색 -> email은 중복이 안되도록 할 예정이기에 1개아니면 0개 반환 예상
@@ -65,7 +68,7 @@ const updateAccount =async (email) => {
   console.log("DB request query : " + sql);
 
   try {
-    const [rows, fields] = await connection.execute(sql, ["testNick",email]);
+    const [rows, fields] = await connection.execute(sql, ["testNick", email]);
     if (rows.length) {
       console.log(rows);
       return 1;
@@ -76,13 +79,13 @@ const updateAccount =async (email) => {
   }
 };
 
-const deleteAccount=()=>{
+const deleteAccount = () => {
 
 };
 
 module.exports = {
-    CreateAccount,
-    readAccount,
-    updateAccount,
-    deleteAccount
+  CreateAccount,
+  readAccount,
+  updateAccount,
+  deleteAccount
 };
--- routes/register.js
+++ routes/register.js
@@ -1,16 +1,33 @@
 var express = require('express');
 var router = express.Router();
-const { CreateAccount , readAccount,updateAccount,deleteAccount} = require('../etc/database');
+const {
+    CreateAccount,
+    readAccount,
+    updateAccount,
+    deleteAccount
+} = require('../etc/database');
 
-router.post('/', async function(req, res, next) {
+router.post('/', async function (req, res, next) {
     console.log(req.body);
-    const {name,phoneNum,birth,email,password} = req.body;
-    const result = await CreateAccount([email,phoneNum,name,password,1,birth]);
+    const {
+        name,
+        phoneNum,
+        birth,
+        email,
+        password
+    } = req.body;
+    const result = await CreateAccount([email, phoneNum, name, password, birth]);
 
-    if(result == 0){
-        res.status(401).json({result: 401, message: '회원가입 실패 ,다시 시도하세요.'});
-    }else{
-        res.status(200).json({result: 200,message: "회원가입 성공"});
+    if (result == 0) {
+        res.status(401).json({
+            result: 401,
+            message: '회원가입 실패 ,다시 시도하세요.'
+        });
+    } else {
+        res.status(200).json({
+            result: 200,
+            message: "회원가입 성공"
+        });
     }
 });
 
